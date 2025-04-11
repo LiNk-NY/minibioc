68c0b92ce494c282fc247b0cc2df1343f489c4b5
LiNK-NY <marcel.ramos@sph.cuny.edu>
2025-04-11 23:46:24 +0000

    add minibioc_clean

---
+++ R/minibioc_clean.R
@@ -0,0 +1,25 @@
+#' @export
+minibioc_clean <- function(
+    version = BiocManager::version(),
+    bin_path = local_bin_repo(),
+    dry.run = TRUE
+) {
+    old <- setwd(bin_path)
+    on.exit(setwd(old))
+
+    repos <- .worker_repositories(version)
+    db <- available.packages(repos = repos)
+    ## software package dependencies
+    contrib_url <- contrib.url(repos[["BioCsoft"]])
+    idx <- db[, "Repository"] == contrib_url
+    software_pkgs <- rownames(db)[idx]
+
+    tars <- list.files(bin_path, pattern = "\\.tar\\.gz$")
+    pkgs <- vapply(strsplit(tars, "_", fixed = TRUE), `[[`, character(1L), 1L)
+    ext.pkgs <- tars[!pkgs %in% software_pkgs]
+    ext.outs <- paste0(pkgs, ".out")
+    if (dry.run)
+        sort(c(ext.pkgs, ext.outs))
+    else
+        file.remove(ext.pkgs, ext.outs)
+}
