205493170313134b5f1e4bb42772b9b850ebdc5d
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-06-03 17:38:00 -0400

    add REPOSITORY file and run write_PACKAGES

--- R/minibioc_install.R
+++ R/minibioc_install.R
@@ -23,6 +23,18 @@
 #' @returns `install_build_binary()` returns invisibly
 #'
 #' @examplesIf interactive()
+#' ver_name <-
+#'     BiocManager:::.version_field("BiocStatus") |> as.character()
+#' repo_file <- file.path(
+#'     local_src_repo(contrib.url = FALSE), "REPOSITORY"
+#' )
+#' if (!file.exists(repo_file))
+#'     download.file(
+#'         url = glue::glue(
+#'             "https://bioconductor.org/packages/{ver_name}/bioc/REPOSITORY"
+#'         ),
+#'         destfile = repo_file
+#'     )
 #' ## Point to source package directories
 #' source_base_dir <- "~/bioc"
 #' bioc_sub_pkgs <- file.path(
@@ -43,6 +55,12 @@
 #'         log_path = local_bin_log()
 #'     )
 #' }
+#'
+#' ## Create PACKAGES, PACKAGES.gz, PACAKGES.rds for the local binary repository
+#' tools::write_PACKAGES(local_bin_repo(), addFiles = TRUE, verbose = TRUE)
+#'
+#' ## Create PACKAGES, PACKAGES.gz, PACAKGES.rds for the local source repository
+#' tools::write_PACKAGES(local_src_repo(), addFiles = TRUE, verbose = TRUE)
 #'
 #' @export
 install_build_binary <-
