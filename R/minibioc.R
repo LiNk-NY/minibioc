b64ec3b1742ab2443c202eca37de996f4733a9dd
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-06-03 14:04:15 -0400

    move minibioc_clean to minibioc.R

--- R/minibioc.R
+++ R/minibioc.R
@@ -120,3 +120,36 @@ minibioc_local <- function(
 
     artifacts$bin_path
 }
+
+#' @rdname minibioc
+#'
+#' @param version `character(1)` The version of the Bioconductor repository
+#'
+#' @param bin_path `character(1)` The directory path of the local binary
+#'   repository
+#'
+#' @param dry.run `logical(1)` Whether to run a dry run (list files to delete
+#'   instead of deleting).
+#'
+#' @export
+minibioc_clean <- function(
+    version = BiocManager::version(),
+    bin_path = local_bin_repo(),
+    dry.run = TRUE
+) {
+    old <- setwd(bin_path)
+    on.exit(setwd(old))
+
+    repos <- .worker_repositories(version)
+    db <- available.packages(repos = repos)
+    ## software package dependencies
+    contrib_url <- contrib.url(repos[["BioCsoft"]])
+    idx <- db[, "Repository"] == contrib_url
+    software_pkgs <- rownames(db)[idx]
+
+    tars <- list.files(bin_path, pattern = "\\.tar\\.gz$")
+    pkgs <- vapply(strsplit(tars, "_", fixed = TRUE), `[[`, character(1L), 1L)
+    ext.pkgs <- tars[!pkgs %in% software_pkgs]
+
+    if (dry.run) sort(ext.pkgs) else file.remove(ext.pkgs)
+}
